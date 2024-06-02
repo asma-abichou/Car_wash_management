eab8bc065f994006defa87d55fe0f385e2082c4b
asma-abichou <abichou.asmaa@gmail.com>
2024-06-02 02:30:14 +0100

    fix tamplate

--- src/Controller/SideClientHomeController.php
+++ src/Controller/SideClientHomeController.php
@@ -7,8 +7,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[Route('/dashboard')]
-#[IsGranted('ROLE_USER')]
+
+
 class SideClientHomeController extends AbstractController
 {
     #[Route('/home-client-side', name: 'client_home')]
